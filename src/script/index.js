8041b507f626bdfafb33556702611bb7b0ef2aa8
Martha <sylvianwaeze@gmail.com>
2020-11-09 10:03:13 +0200

    Still Developing

---
+++ src/script/index.js
@@ -0,0 +1,5 @@
+const $like = document.querySelector("#like");
+$like.addEventListener("click", () => ($like.style.color = "red"));
+document.addEventListener('DOMContentloaded', (Event) => {
+ console.log('DOM fully loadede and parsed');
+});
